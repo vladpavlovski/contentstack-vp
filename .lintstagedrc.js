913ddca89e81c5a50fcbd954036b00c8ed8d5816
Vladislav Pavlovski <vladislav.pavlovski@gmail.com>
2024-06-04 14:36:01 +0200

    fix lint

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -5,8 +5,6 @@ const path = require('path')
 const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')} `
 
-const buildPrettierCommand = `prettier --write`
-
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand, buildPrettierCommand],
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
 }
